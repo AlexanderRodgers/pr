44c4f49bee4be65669de9cce8b6eb41cf9ccdf52
Alex Rodgers <alexedrodgers@gmail.com>
2019-02-02 10:20:21 -0800

    Refactoring front end for nuxt implementation

---
+++ ratings/components/Search.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <v-autocomplete
+            v-model="search"
+            :items="professors"
+            placeholder="Search for a Professor"
+            append-icon="search"
+            item-text="first_last"
+            item-value="id"
+            @click:append="search"
+        >
+
+        <template
+        slot="item"
+        slot-scope="{ item, tile }"
+      >
+        <v-list-tile-avatar
+          color="indigo"
+          class="headline font-weight-light white--text"
+        >
+          {{ item.first_name.charAt(0) }}
+        </v-list-tile-avatar>
+        <v-list-tile-content>
+          <v-list-tile-title v-text="item.first_last"></v-list-tile-title>
+          <v-list-tile-sub-title v-text="item.major"></v-list-tile-sub-title>
+        </v-list-tile-content>
+        <v-list-tile-action>
+          <v-icon>face  </v-icon>
+        </v-list-tile-action>
+      </template>
+
+        </v-autocomplete>
+
+        <template slot="no-data">
+            <v-list-tile>
+                <v-list-tile-title>
+                    Search for a Professor
+                </v-list-tile-title>
+            </v-list-tile>
+        </template>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            search: '',
+            dev: 'http://localhost:8000',
+            professors: [],
+        }
+    },
+
+    methods: {
+        runSearch() {
+            console.log('Search function ran.')
+        }
+    },
+
+    mounted() {
+        axios.get(this.dev + '/api/reviews/')
+            .then(res => {
+                for (let prof of res.data) {
+                    prof['first_last'] = prof.first_name + " " + prof.last_name
+                    this.professors.push(prof)
+                }
+            })
+            .catch(e => {
+                console.error(e)
+            })
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ ratings/pages/test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <new-professor/>
+    </div>
+</template>
+
+<script>
+import NewProfessor from '~/components/NewProfessor'
+export default {
+    components: {
+        NewProfessor,
+    }
+}
+</script>
+
+<style>
+
+</style>

28e9ac4b3e085569942469cacb4c3a867bcf272e
Alexander Rodgers <alexedrodgers@gmail.com>
2019-02-07 01:19:23 -0800

    added secret functionality

--- ratings/pages/test.vue
+++ ratings/pages/test.vue
@@ -12,6 +12,40 @@ export default {
     components: {
         Search,
         NewProfessor,
+    },
+
+    data() {
+        return {
+            keys: '',
+            codeEntered: false,
+        }
+    },
+
+    methods: {
+        getCode(event) {
+            this.keys += event.keyCode;
+            //Flush for efficiency
+            if (this.keys.length > 100) {
+                this.keys = ''
+            }
+            console.log(this.keys)
+            this.codeEntered = this.keys.includes('3838404037393739666513')
+        }
+    },
+
+    watch: {
+        codeEntered() {
+            window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        }
+    },
+
+    mounted() {
+        window.addEventListener("keypress", e => {
+            this.getCode
+        });
+        window.addEventListener("keyup", e => {
+            this.getCode(e)
+        });
     }
 }
 </script>

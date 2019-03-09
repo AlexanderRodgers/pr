1f2c2c60ba2ab9237a41b318bbc66f9a56155e9c
Alex Rodgers <alex.e.rodgers@gmail.com>
2019-03-08 16:02:45 -0800

    Added a the NewProfessor component as a button and updated the index file

--- ratings/components/NewProfessor.vue
+++ ratings/components/NewProfessor.vue
@@ -1,63 +1,67 @@
 <template>
-    <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-            <v-card class="elevation-12">
-                <v-toolbar dark color="primary title">Add a Professor</v-toolbar>
-                <v-form ref="form" v-model="valid" lazy-validation>
-                    <v-flex class="form-field">
-                        <v-text-field
-                        v-model="firstName"
-                        label="First Name"
-                        :rules="[formRules.hasContent(firstName), formRules.hasNumber(lastName)]"
-                        required
-                        outline
-                        />
-                    </v-flex>
-                    <v-flex class="form-field">
-                        <v-text-field
-                        v-model="lastName"
-                        label="Last Name"
-                        :rules="[formRules.hasContent(lastName), formRules.hasNumber(lastName)]"
-                        required
-                        outline
-                        />
-                    </v-flex>
-                    <v-flex class="form-field">
-                        <v-text-field
-                        v-model="email"
-                        label="Professor Email"
-                        suffix="@calpoly.edu"
-                        outline
-                        hint="This field is optional"
-                        />
-                    </v-flex>
-                    <v-flex class="form-field">
-                        <v-select
-                        v-model="major"
-                        :items=majorList
-                        label="Major"
-                        item-text="major"
-                        item-value="id"
-                        outline
-                        />
-                    </v-flex>
-                    <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn @click="validate()" color="info">Submit</v-btn>
-                    </v-card-actions>    
-                </v-form>
-            </v-card>
-        </v-layout>
-
-    </v-container>
+    <div>
+        <v-btn fab color="primary" @click="dialog = true">
+            <v-icon>group_add</v-icon>
+        </v-btn>
+        <!-- TODO: change max-width to fit to all screens better -->
+    <v-dialog v-model="dialog" max-width="35%">
+        <v-card class="elevation-12">
+            <v-toolbar dark color="primary title">Add a Professor</v-toolbar>
+            <v-form ref="form" v-model="valid" lazy-validation>
+                <v-flex class="form-field">
+                    <v-text-field
+                    v-model="firstName"
+                    label="First Name"
+                    :rules="[formRules.hasContent(firstName), formRules.hasNumber(lastName)]"
+                    required
+                    outline
+                    />
+                </v-flex>
+                <v-flex class="form-field">
+                    <v-text-field
+                    v-model="lastName"
+                    label="Last Name"
+                    :rules="[formRules.hasContent(lastName), formRules.hasNumber(lastName)]"
+                    required
+                    outline
+                    />
+                </v-flex>
+                <v-flex class="form-field">
+                    <v-text-field
+                    v-model="email"
+                    label="Professor Email"
+                    suffix="@calpoly.edu"
+                    outline
+                    hint="This field is optional"
+                    />
+                </v-flex>
+                <v-flex class="form-field">
+                    <v-select
+                    v-model="major"
+                    :items=majorList
+                    label="Major"
+                    item-text="major"
+                    item-value="id"
+                    outline
+                    />
+                </v-flex>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn @click="validate()" color="primary">Add</v-btn>
+                </v-card-actions>    
+            </v-form>
+        </v-card>
+    </v-dialog>
+    </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
-
+    
     data() {
         return {
+            dialog: false,
             valid: true,
             dev_mode: true,
             firstName: '',
@@ -123,6 +127,10 @@ export default {
     margin-left: 20px;
     margin-right: 20px;
     margin-top: 20px;
+}
+
+.v-btn {
+
 }
 
 </style>
--- ratings/pages/index.vue
+++ ratings/pages/index.vue
@@ -1,17 +1,33 @@
 <template>
-  <div>
-    <search/>
-  </div>
+  <v-container fill-height>
+      <v-layout row align-center class="flex-container">
+        <search fluid class="flex-item"/>
+        <new-professor class="flex-item"/>
+      </v-layout>
+  </v-container>
 </template>
 
 <script>
 import profs from '~/mixins/profs.js'
 import Search from '~/components/Search'
+import NewProfessor from '~/components/NewProfessor'
 export default {
   components: {
     Search,
+    NewProfessor,
   },
 
   mixins: [profs],
 }
 </script>
+
+<style scoped>
+.flex-container {
+  display: flex;
+}
+
+.flex-item:nth-of-type(1) {
+  flex-grow: 2 !important;
+}
+
+</style>
